56fe06b4f41fe2b6c1b3ed646697d0d8a724306e
Trottie McQueen <mcqueen.trottie007@gmail.com>
2023-11-24 16:44:50 -0500

    building components

--- face-image-recognition-smartbrain-app/src/components/Logo/Logo.js
+++ face-image-recognition-smartbrain-app/src/components/Logo/Logo.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import Tilt from 'react-parallax-tilt';
 
-
+const Logo = () => {
+  return (
+    <Tilt>
+      <div style={{ height: '300px', backgroundColor: 'darkgreen' }}>
+        <h1>React Parallax Tilt 👀</h1>
+      </div>
+    </Tilt>
+  );
+};
 
 export default Logo;
