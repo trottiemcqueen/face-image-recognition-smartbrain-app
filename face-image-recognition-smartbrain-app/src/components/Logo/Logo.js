e5d38706290143754b3e942a487827dff86f0de4
Trottie McQueen <mcqueen.trottie007@gmail.com>
2023-11-29 22:03:45 -0500

    updated logo now working on the engine

--- face-image-recognition-smartbrain-app/src/components/Logo/Logo.js
+++ face-image-recognition-smartbrain-app/src/components/Logo/Logo.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Tilt from 'react-parallax-tilt';
-import brain1 from './brain1.png';
+import brainy3 from './brainy3.png';
 import './Logo.css';
 
 const Logo = () => {
   return (
     <div className='ma4 mt0'>
-      <Tilt className="Tilt br2 shadow-2" options={{ max : 75 }} style={{ width : 150, height : 150 }}>
+      <Tilt className="Tilt br2 shadow-2" options={{ max : 75 }} style={{ width : 300, height : 175 }}>
           <div className="Tilt-inner pa3">
-            <img style={{paddingTop: '5px'}} alt='logo' src={brain1}/>
+            <img style={{paddingTop : '2px'}} alt='logo' src={brainy3}/>
           </div>
       </Tilt>
     </div>
