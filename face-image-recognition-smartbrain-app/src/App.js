42c96846e2e629a7f26b8a544dc8de19de08132a
Trottie McQueen <mcqueen.trottie007@gmail.com>
2023-11-30 21:31:27 -0500

    added clarifai api

--- face-image-recognition-smartbrain-app/src/App.js
+++ face-image-recognition-smartbrain-app/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import clarifai = require('clarifai');
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
@@ -7,7 +8,25 @@ import ParticlesBg from 'particles-bg';
 import './App.css';
 
 
+const app = new clarifai.App({
+  PAT: ''
+});
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    console.log('click');
+  }
   render () {
       return (
       
@@ -16,8 +35,11 @@ class App extends Component {
           <Navigation />
           <Logo />
           <Rank />
-          <ImageLinkForm />
-                 {/*} <FaceRecognition />*/}
+          <ImageLinkForm 
+            onInputChange={this.onInputChange} 
+            onButtonSubmit={this.onButtonSubmit}/>
+          <clarifai />
+                {/*<FaceRecognition />*/}
         </div>
         
       
--- face-image-recognition-smartbrain-app/src/components/ImageLinkForm/ImageLinkForm.js
+++ face-image-recognition-smartbrain-app/src/components/ImageLinkForm/ImageLinkForm.js
@@ -3,7 +3,7 @@ import './ImageLinkForm.css';
 
 
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
   return (
     <div>
       <p className='f3'>
@@ -11,8 +11,11 @@ const ImageLinkForm = () => {
       </p>
       <div className='center'>
         <div className='form center pa4 br3 shadow-5'>
-        <input className='f4 pa2 w-70 center' type='tex' />
-        <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-red'>Detect</button>
+        <input className='f4 pa2 w-70 center' type='tex' onChange={onInputChange}/>
+        <button 
+          className='w-30 grow f4 link ph3 pv2 dib white bg-light-red'
+          onClick={onButtonSubmit}
+        >Detect</button>
         </div>
       </div>
     </div>
