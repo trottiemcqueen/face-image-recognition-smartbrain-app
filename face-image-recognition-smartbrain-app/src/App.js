4852f9b9c92e984b23ba8f9c049dd91180f88467
Trottie McQueen <mcqueen.trottie007@gmail.com>
2023-12-02 16:32:15 -0500

    added FacialRecognition component

--- face-image-recognition-smartbrain-app/src/App.js
+++ face-image-recognition-smartbrain-app/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import clarifai = require('clarifai');
+import clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
@@ -25,21 +26,33 @@ class App extends Component {
   }
 
   onButtonSubmit = () => {
-    console.log('click');
+    this.setState({imageUrl: input});
+    app.models.predict(clarifai.COLOR_MODEL, "https://samples.clarifai.com/face-det.jpg")
+    .then(function(response) {
+      console.log(response);
+    },
+    function(err) {
+        //this was an error
+      }
+    );
   }
+
+
   render () {
       return (
       
         <div className="App">
-          <ParticlesBg type="cobweb" bg={true} />
+          <ParticlesBg type="cobweb" 
+          bg={true} 
+          />
           <Navigation />
           <Logo />
           <Rank />
           <ImageLinkForm 
             onInputChange={this.onInputChange} 
-            onButtonSubmit={this.onButtonSubmit}/>
-          <clarifai />
-                {/*<FaceRecognition />*/}
+            onButtonSubmit={this.onButtonSubmit}
+          />
+          <FaceRecognition imageUrl={this.state.imageUrl} />
         </div>
         
       
